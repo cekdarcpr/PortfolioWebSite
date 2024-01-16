978734d6cea957faaf303f7345626c7195ffd511
cekdarcpr <cekooo12@gmail.com>
2024-01-17 00:21:46 +0300

    Add project files.

---
+++ DataLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using KisiselWeb.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace KisiselWeb.DataLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-3RDN6LK\\SQLEXPRESS;database=KisiselWebDb;integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
